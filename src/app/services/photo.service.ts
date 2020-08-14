658cd15a1155e59401357a66d55dcaba55f7bf47
Brian Lee <BrianL743@gmail.com>
2020-08-14 17:02:45 -0400

    added photoService

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
-
+import { Photo } from '../models/photo';
+import {HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  async createPhoto(photo:Photo, uId:number):Promise<Photo>{
+    photo = await this.http.post<Photo>(`http://localhost:8080/user/${uId}/photos`, photo).toPromise();
+    return photo;
+  }
+
+  async getPhotos(uId:number, hashtag:string):Promise<Array<Photo>>{
+    const books:Array<Photo> = await this.http.get<Array<Photo>>(`http://localhost:8080/user/${uId}/photos?hashtag=${hashtag}`).toPromise();
+    return books;
+  }
+
+  async getPhotoById(pId:number):Promise<Photo>{
+    const photo:Photo = await this.http.get<Photo>(`http://localhost:8080/user/0/photos/${pId}`).toPromise();
+    return photo;
+  }
+
+  async editPhoto(photo:Photo):Promise<Photo>{
+    photo = this.http.put<Photo>("http://localhost:8080/users/0/photos", photo).toPromise();
+    return photo;
+  }
+
+  async deletePhoto(pId:number):Promise<void>{
+    await this.http.delete<void>(`http://localhost:8080/users/0/photos/${pId}`).toPromise();
+  }
+
 }
