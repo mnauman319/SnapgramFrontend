657bda735a54fc2abd789c9f4977705be8b398fd
Kurt Dawiec <kurtmdawiec@gmail.com>
2020-08-17 13:31:44 -0400

    homepage html done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatBadgeModule} from '@angular/material/badge';
 
 
 import { AppComponent } from './app.component';
@@ -49,7 +51,10 @@ import { PhotoService } from './services/photo.service';
     MatButtonModule,
     MatGridListModule,
     MatDividerModule, 
-    MatIconModule
+    MatIconModule,
+    MatToolbarModule,
+    MatBadgeModule,
+    MatExpansionModule
   ],
   providers: [UserService,TagService,PhotoService],
   bootstrap: [AppComponent]
